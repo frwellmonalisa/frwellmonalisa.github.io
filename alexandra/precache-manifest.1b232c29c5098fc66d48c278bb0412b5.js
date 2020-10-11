e6e7a66ef9928362d632010fba90c29086e623e4
Andrey Margiev <ellenripley4@yandex.ru>
2020-10-11 17:12:56 +0300

    Калькулятор для патентного поверенного

---
+++ alexandra/precache-manifest.1b232c29c5098fc66d48c278bb0412b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c68a7ac845907ab56ab9d00cec070636",
+    "url": "/alexandra/index.html"
+  },
+  {
+    "revision": "a56660aedeabc82795c3",
+    "url": "/alexandra/static/css/2.9f4ea2fb.chunk.css"
+  },
+  {
+    "revision": "a56660aedeabc82795c3",
+    "url": "/alexandra/static/js/2.c43ac4a7.chunk.js"
+  },
+  {
+    "revision": "94a3bfaba5b0bd866a3e56687327b9ad",
+    "url": "/alexandra/static/js/2.c43ac4a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53466e0b37e05fb66dc6",
+    "url": "/alexandra/static/js/main.a0027956.chunk.js"
+  },
+  {
+    "revision": "50c7ea81fbac696888e5",
+    "url": "/alexandra/static/js/runtime-main.0e4878ee.js"
+  }
+]);
